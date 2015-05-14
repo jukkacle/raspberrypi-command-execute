bc437a42fe73b4220a53848f2d627d82d50e44f2
Shibu T Narayanan <shibu.narayanan@oracle.com>
2015-05-14 18:21:07 +0530

    a

--- src/main/java/com/vypeensoft/raspberrypi_command_execute/MainActivity.java
+++ src/main/java/com/vypeensoft/raspberrypi_command_execute/MainActivity.java
@@ -5,15 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
+import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.content.Context;
+ 
+ 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.ByteArrayOutputStream;
+
+import com.jcraft.jsch.*;
 
 public class MainActivity extends Activity {
 
@@ -47,6 +56,29 @@ public class MainActivity extends Activity {
       public void onClick(View v) {
         int id = v.getId();
         setTitle("button_" + (id+id));
+        try {
+            //executeRemoteCommand("pi","password","192.168.1.10",22);
+            List<String> list = FileUtil.readFileContentsAsStringList(MainActivity.this, getString(R.string.command_file_name));
+            setTitle("1");
+
+            Command currentCommand = null;
+            for (int i = 0; i < list.size(); i++) {
+            setTitle("2=="+i);
+                String oneLine = list.get(i);
+                Command co = new Command(oneLine);
+                setTitle("3=="+co.id +","+id);
+                if(co.id == id) {
+                    setTitle("4");
+                    currentCommand = co;
+                }
+            }
+            if(true) throw new RuntimeException("Shibu");
+            setTitle("button_" + currentCommand.commandString);
+            showToast("button_" + currentCommand.commandString);
+        } catch(Exception e) {
+            //setTitle("error");
+            showToast("error="+e.getMessage());
+        }
       }
     };
 
@@ -63,33 +95,34 @@ public class MainActivity extends Activity {
         button08 = (Button) findViewById(R.id.button08);    button08.setOnClickListener(listener1);    button08.setVisibility(View.INVISIBLE);    buttonList.add(button08);
         button09 = (Button) findViewById(R.id.button09);    button09.setOnClickListener(listener1);    button09.setVisibility(View.INVISIBLE);    buttonList.add(button09);
         button10 = (Button) findViewById(R.id.button10);    button10.setOnClickListener(listener1);    button10.setVisibility(View.INVISIBLE);    buttonList.add(button10);
-        button11 = (Button) findViewById(R.id.button11);    button11.setOnClickListener(listener1);    button11.setVisibility(View.INVISIBLE);    buttonList.add(button11);
-        button12 = (Button) findViewById(R.id.button12);    button12.setOnClickListener(listener1);    button12.setVisibility(View.INVISIBLE);    buttonList.add(button12);
-        button13 = (Button) findViewById(R.id.button13);    button13.setOnClickListener(listener1);    button13.setVisibility(View.INVISIBLE);    buttonList.add(button13);
-        button14 = (Button) findViewById(R.id.button14);    button14.setOnClickListener(listener1);    button14.setVisibility(View.INVISIBLE);    buttonList.add(button14);
-        button15 = (Button) findViewById(R.id.button15);    button15.setOnClickListener(listener1);    button15.setVisibility(View.INVISIBLE);    buttonList.add(button15);
-        button16 = (Button) findViewById(R.id.button16);    button16.setOnClickListener(listener1);    button16.setVisibility(View.INVISIBLE);    buttonList.add(button16);
-        button17 = (Button) findViewById(R.id.button17);    button17.setOnClickListener(listener1);    button17.setVisibility(View.INVISIBLE);    buttonList.add(button17);
-        button18 = (Button) findViewById(R.id.button18);    button18.setOnClickListener(listener1);    button18.setVisibility(View.INVISIBLE);    buttonList.add(button18);
-        button19 = (Button) findViewById(R.id.button19);    button19.setOnClickListener(listener1);    button19.setVisibility(View.INVISIBLE);    buttonList.add(button19);
-        button20 = (Button) findViewById(R.id.button20);    button20.setOnClickListener(listener1);    button20.setVisibility(View.INVISIBLE);    buttonList.add(button20);
+        //button11 = (Button) findViewById(R.id.button11);    button11.setOnClickListener(listener1);    button11.setVisibility(View.INVISIBLE);    buttonList.add(button11);
+        //button12 = (Button) findViewById(R.id.button12);    button12.setOnClickListener(listener1);    button12.setVisibility(View.INVISIBLE);    buttonList.add(button12);
+        //button13 = (Button) findViewById(R.id.button13);    button13.setOnClickListener(listener1);    button13.setVisibility(View.INVISIBLE);    buttonList.add(button13);
+        //button14 = (Button) findViewById(R.id.button14);    button14.setOnClickListener(listener1);    button14.setVisibility(View.INVISIBLE);    buttonList.add(button14);
+        //button15 = (Button) findViewById(R.id.button15);    button15.setOnClickListener(listener1);    button15.setVisibility(View.INVISIBLE);    buttonList.add(button15);
+        //button16 = (Button) findViewById(R.id.button16);    button16.setOnClickListener(listener1);    button16.setVisibility(View.INVISIBLE);    buttonList.add(button16);
+        //button17 = (Button) findViewById(R.id.button17);    button17.setOnClickListener(listener1);    button17.setVisibility(View.INVISIBLE);    buttonList.add(button17);
+        //button18 = (Button) findViewById(R.id.button18);    button18.setOnClickListener(listener1);    button18.setVisibility(View.INVISIBLE);    buttonList.add(button18);
+        //button19 = (Button) findViewById(R.id.button19);    button19.setOnClickListener(listener1);    button19.setVisibility(View.INVISIBLE);    buttonList.add(button19);
+        //button20 = (Button) findViewById(R.id.button20);    button20.setOnClickListener(listener1);    button20.setVisibility(View.INVISIBLE);    buttonList.add(button20);
 
-        
-        String oneLine1 = "|1|Rasp 1 - restart ytdl|/home/pi/Youtube/Youtube_restart.sh";
+        /////////////////////////////////////////////////////////////////////////////////////
+        String oneLine1 = "";
+        oneLine1 = "|1|Rasp 1 - /home/pi/Youtube/Youtube_restart.sh|/home/pi/Youtube/Youtube_restart.sh";
         FileUtil.writeStringToNewFile(this, getString(R.string.command_file_name), oneLine1);
 
-        oneLine1 = "|2|Rasp 2 - restart deluge|/home/pi/Deluge_restart.sh";
+        oneLine1 = "|2|Rasp 2|/home/pi/Deluge_restart.sh";
         FileUtil.appendStringToFile(this, getString(R.string.command_file_name), oneLine1);
 
-        oneLine1 = "|3|Rasp 3 - restart Curl|/home/pi/Curl_restart.sh";
+        oneLine1 = "|3|Rasp 3|/home/pi/Curl_restart.sh";
         FileUtil.appendStringToFile(this, getString(R.string.command_file_name), oneLine1);
 
-        oneLine1 = "|4|Rasp 4 - restart Curl|/home/pi/Curl_restart.sh";
+        oneLine1 = "|4|Rasp 4|/home/pi/Curl_restart.sh";
         FileUtil.appendStringToFile(this, getString(R.string.command_file_name), oneLine1);
 
-        oneLine1 = "|5|Rasp 5 - restart Curl|/home/pi/Curl_restart.sh";
+        oneLine1 = "|5|Rasp 5|/home/pi/Curl_restart.sh";
         FileUtil.appendStringToFile(this, getString(R.string.command_file_name), oneLine1);
-        
+        /////////////////////////////////////////////////////////////////////////////////////
         
         List<String> list = FileUtil.readFileContentsAsStringList(this, getString(R.string.command_file_name));
 
@@ -100,38 +133,7 @@ public class MainActivity extends Activity {
             currentButton.setVisibility(View.VISIBLE);
             currentButton.setId(co.id);
             currentButton.setText(co.label);
-            //currentButton.setId(i);
-            //currentButton.setText(oneLine);
-            
-            
-//            if(i == 1) {
-//                button01.setVisibility(View.VISIBLE);
-//                button01.setId(i);
-//                button01.setText(oneLine);
-//            }
-//            //if(i == 1) {
-//            //    button01.setVisibility(View.VISIBLE);
-//            //    button01.setId(co.id);
-//            //    button01.setText(co.commandString);
-//            //}
-//            if(i == 2) {
-//                button02.setVisibility(View.VISIBLE);
-//                button02.setId(i);
-//                button02.setText("Rasp 1 - restart deluge");
-//                button02.setText(oneLine);
-//            }
-//            if(i == 3) {
-//                button03.setVisibility(View.VISIBLE);
-//                button03.setId(i);
-//                button03.setText("Rasp 2 - restart Curl");
-//                button03.setText(oneLine);
-//            }
         }
-            
-
-       //FileUtil.readFileContentsAsString(this, getString(R.string.command_file_name));
-       //FileUtil.readFileContentsAsStringList(this, getString(R.string.command_file_name));
-       
        setTitle("file write true" );
     } catch (Exception ioe) {
         ioe.printStackTrace();
@@ -140,4 +142,46 @@ public class MainActivity extends Activity {
     
 
   } //end of onCreate()
+  
+  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+     public static String executeRemoteCommand(String username,String password,String hostname,int port) throws Exception {
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(username, hostname, port);
+        session.setPassword(password);
+
+        // Avoid asking for key confirmation
+        Properties prop = new Properties();
+        prop.put("StrictHostKeyChecking", "no");
+        session.setConfig(prop);
+
+        session.connect();
+
+        // SSH Channel
+        ChannelExec channelssh = (ChannelExec) session.openChannel("exec");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        channelssh.setOutputStream(baos);
+
+        //Execute command
+        channelssh.setCommand("/home/pi/rasp_test.sh");
+        channelssh.connect();
+        channelssh.disconnect();
+
+        return baos.toString();
+    }  
+    
+    public void showToast(String msg) {
+        Context context = getApplicationContext();
+        CharSequence text = msg;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+   }
+    
 }
