7ed8e6b7571bd262a5c614eca15e0c273c4da27a
Shibu TN <tnshibu@yahoo.com>
2018-09-13 03:25:52 +0530

    .travis.yml

---
+++ upload_apk.sh
@@ -0,0 +1,11 @@
+cd $ HOME 
+git config --global user.email "shibu@shibu.com" 
+git config --global user.name "Shibu Travis" 
+
+git clone https://jukkacle:$GITHUB_API_KEY@github.com/jukkacle/raspberrypi-command-execute
+cd raspberrypi-command-execute
+cp "/home/travis/.m2/repository/com/vypeensoft/RaspberryPi_Command_Execute/1.0/RaspberryPi_Command_Execute-1.0.apk" releases
+git add releases/*
+git commit -m "Travis build $ TRAVIS_BUILD_NUMBER commited APK [skip ci] " 
+git push origin master
+echo -e" Done \ n "
